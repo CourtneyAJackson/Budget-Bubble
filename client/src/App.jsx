282c7d4163e889984fec99625730f99a1ce4f5b2
Courtney Jackson <courtneyjackson@Courtneys-MacBook-Air.local>
2021-08-23 12:39:02 -0700

    routes and links

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -49,6 +49,7 @@ function App() {
       <Route path="/new">
         {
           bills.map((bill, index) => {
+            
             return (
               <>
                 <h3>Name: {bill.fields.name}</h3>
